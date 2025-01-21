b2f7c9ad1cc716328da4b1296a876aa47264bb8a
Terhon <terhon95@gmail.com>
2025-01-21 20:27:52 +0100

    test controller

---
+++ StorageWeb/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StorageWeb.Controllers;
+
+public class TestController : Controller
+{
+    // 
+    // GET: /Test/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /Test/Welcome/ 
+    public string Welcome(string name, int id = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+    }
+}
