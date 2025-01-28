f976d27fc150abf728d213a1e2fbc7243494a654
Terhon <terhon95@gmail.com>
2025-01-28 20:52:24 +0100

    item validation

--- StorageWeb/Models/Item.cs
+++ StorageWeb/Models/Item.cs
@@ -6,17 +6,27 @@ namespace StorageWeb.Models;
 public class Item
 {
     public int Id { get; set; }
-    
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Name { get; set; }
-    
+
     [Display(Name = "Acquisition Date")]
-    [DataType(DataType.Date)] 
+    [DataType(DataType.Date)]
     public DateTime AcquisitionDate { get; set; }
-    
-    public string? Type { get; set; }
-    
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string? Type { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
     public string? Rating { get; set; }
 }
