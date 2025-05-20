2864927e199a8ec92844f65386966f039f549d54
Terhon <terhon95@gmail.com>
2025-05-20 23:45:57 +0200

    ItemController tests

--- StorageWeb.Tests/ItemControllerTests.cs
+++ StorageWeb.Tests/ItemControllerTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -44,7 +45,7 @@ public class ItemControllerTests
     }
 
     [Fact]
-    public async Task Details_ReturnsViewResult()
+    public async Task Details_ReturnsViewResult_WhenItemExists()
     {
         var context = GetInMemoryStorageWebContext();
         var controller = new ItemController(context);
@@ -55,5 +56,77 @@ public class ItemControllerTests
         var model = Assert.IsAssignableFrom<Item>(viewResult.ViewData.Model);
         model.Name.Should().Be("Item 1");
         model.Unit.Should().Be("unit 1");
+    }
+
+    [Fact]
+    public async Task Create_AddsItemToItemController_WhenValid()
+    {
+        var context = GetInMemoryStorageWebContext();
+        var controller = new ItemController(context);
+        
+        var newItem = new Item { Name = "New Item", Unit = "unit 1" };
+        var result = await controller.Create(newItem);
+        
+        Assert.IsType<RedirectToActionResult>(result);
+        context.Item.Count().Should().Be(3);
+        context.Item.Should().Contain(newItem);
+    }
+    
+    [Fact]
+    public async Task Create_AddsItemToItemController_WhenInvalid()
+    {
+        var context = GetInMemoryStorageWebContext();
+        var controller = new ItemController(context);
+        
+        var newItem = new Item{ Name = new string('x', 61), Unit = new string('u', 21) };
+        
+        var validationResults = new List<ValidationResult>();
+        Validator.TryValidateObject(newItem, new ValidationContext(newItem), validationResults, true);
+        foreach (var valResult in validationResults)
+            foreach (var memberName in valResult.MemberNames)
+                controller.ModelState.AddModelError(memberName, valResult.ErrorMessage ?? "Invalid");
+        
+        var result = await controller.Create(newItem);
+        
+        Assert.IsType<ViewResult>(result);
+    }
+    
+    [Fact]
+    public async Task Edit_ReturnsViewResult_WhenItemExists()
+    {
+        var context = GetInMemoryStorageWebContext();
+        var controller = new ItemController(context);
+        
+        var result = await controller.Edit(1);
+        
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<Item>(viewResult.ViewData.Model);
+        model.Name.Should().Be("Item 1");
+        model.Unit.Should().Be("unit 1");
+    }
+
+    [Fact]
+    public async Task Delete_ReturnsViewResult_WhenItemExists()
+    {
+        var context = GetInMemoryStorageWebContext();
+        var controller = new ItemController(context);
+        
+        var result = await controller.Delete(1);
+        var viewResult = Assert.IsType<ViewResult>(result);
+            
+        var model = Assert.IsAssignableFrom<Item>(viewResult.ViewData.Model);
+        model.Name.Should().Be("Item 1");
+        model.Unit.Should().Be("unit 1");
+    }
+
+    [Fact]
+    public void Create_ReturnsViewResult()
+    {
+        var context = GetInMemoryStorageWebContext();
+        var controller = new ItemController(context);
+        
+        var result = controller.Create();
+        
+        Assert.IsType<ViewResult>(result);
     }
 }
