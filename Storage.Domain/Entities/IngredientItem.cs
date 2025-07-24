452d091f8f4a1a87595308ab1b34bec874909f62
Terhon <terhon95@gmail.com>
2025-07-24 17:08:45 +0200

    ingredientitem and recipe entities

---
+++ Storage.Domain/Entities/IngredientItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Storage.Domain.Entities;
+
+public class IngredientItem
+{
+    public int Id { get; set; }
+    
+    public Recipe Recipe { get; set; }
+    
+    public ItemType Item { get; set; }
+    
+    public double Amount { get; set; }
+}
---
+++ Storage.Domain/Entities/Recipe.cs
@@ -0,0 +1,12 @@
+﻿namespace Storage.Domain.Entities;
+
+public class Recipe
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+
+    public List<IngredientItem> Ingredients { get; } = [];
+}
