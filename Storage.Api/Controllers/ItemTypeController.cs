bce21ed3e0ddcc610cd7c0cf82597607a31beefd
Terhon <terhon95@gmail.com>
2025-08-04 18:04:42 +0200

    added api project

---
+++ Storage.Api/Controllers/ItemTypeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Storage.Application.ItemType.Commands;
+using Storage.Application.ItemType.Queries;
+
+namespace Storage.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ItemTypeController(IItemTypeCommandService commandService, IItemTypeQueryService queryService): ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var items = await queryService.GetAllItemTypes();
+        return Ok(items);
+    }
+}
---
+++ Storage.Api/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Storage.Infrastructure;
+using Storage.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<StorageDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StorageDbContext") ?? throw new InvalidOperationException("Connection string 'StorageDbContext' not found.")));
+
+builder.Services.AddControllers();
+
+builder.AddInfrastructure();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
