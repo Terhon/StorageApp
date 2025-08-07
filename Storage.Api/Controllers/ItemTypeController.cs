40266d974566c5adaa27014379fed3269b24cea0
Terhon <terhon95@gmail.com>
2025-08-07 22:01:24 +0200

    post and delete api methods for item type

--- Storage.Api/Controllers/ItemTypeController.cs
+++ Storage.Api/Controllers/ItemTypeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Storage.Application.ItemType.Commands;
+using Storage.Application.ItemType.Commands.DTOs;
 using Storage.Application.ItemType.Queries;
 
 namespace Storage.Api.Controllers;
@@ -13,5 +14,19 @@ public class ItemTypeController(IItemTypeCommandService commandService, IItemTyp
     {
         var items = await queryService.GetAllItemTypes();
         return Ok(items);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody]CreateItemTypeCommand itemType)
+    {
+        await commandService.AddItemType(itemType);
+        return CreatedAtAction(nameof(Post), itemType);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await commandService.DeleteItemType(new DeleteItemTypeCommand(id));
+        return NoContent();
     }
 }
