96104c668c0bf93049bdaa363b054aa9d3d9adae
Terhon <terhon95@gmail.com>
2025-08-17 22:45:27 +0200

    storage item controller for api

---
+++ Storage.Api/Controllers/StorageItemController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Storage.Application.StorageItem.Commands;
+using Storage.Application.StorageItem.Commands.DTOs;
+using Storage.Application.StorageItem.Queries;
+
+namespace Storage.Api.Controllers;
+
+public class StorageItemController(IStorageItemCommandService commandService, IStorageItemQueryService queryService): ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var items = await queryService.GetAllStorageItems();
+        return Ok(items);
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var item = await queryService.GetStorageItemById(id);
+        if(item == null)
+            return NotFound();
+        
+        return Ok(item);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody]CreateStorageItemCommand storageItem)
+    {
+        await commandService.AddStorageItem(storageItem);
+        return CreatedAtAction(nameof(Post), storageItem);
+    }
+    
+    [HttpPatch]
+    public async Task<IActionResult> Patch([FromBody]UpdateStorageItemCommand storageItem)
+    {
+        await commandService.UpdateStorageItem(storageItem);
+        return NoContent();
+    }
+    
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await commandService.DeleteStorageItem(new DeleteStorageItemCommand(id));
+        return NoContent();
+    }
+}
